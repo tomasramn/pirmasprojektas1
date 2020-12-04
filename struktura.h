3863944b0f7276541fb6e614247f08dedc6cfa2a
tomasramn <71840077+tomasramn@users.noreply.github.com>
2020-12-04 22:01:02 +0200

    Delete struktura.h

--- struktura.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "funkcijos.h"
-struct studentas {
-	string Vardas, Pavarde;
-	int Pazymiai[DYDIS], num = 0;
-	float Egzaminas, mediana = 0, vidurkis = 0, rez = 0;
-	vector<float> paz;
-};
